56b981a57c67a2328537cbc225f1408f244c02d0
Theo <octheo.quennehen@gmail.com>
2023-11-22 13:18:48 +0100

    route client

--- api/routes/commande.js
+++ api/routes/commande.js
@@ -17,4 +17,23 @@ router.get('/', async (req, res, next) => {
 });
 
 
+router.post('/', async (req, res, next) => {
+    try {
+        const {status,date_commande,adr_livraison,adr_facturation,code_postal,code_postal_facturation,id_client,id_promotion,id_panier } = req.body;
+
+        const conn = await connectToDatabase();
+        const result = await conn.query(
+            'INSERT INTO Commande (status,date_commande,adr_livraison,adr_facturation,code_postal,code_postal_facturation,id_client,id_promotion,id_panier) VALUES (?, ?, ?,?,?,?,?,?,?)',
+            [status,date_commande,adr_livraison,adr_facturation,code_postal,code_postal_facturation,id_client,id_promotion,id_panier]
+        );
+
+        res.status(200).json({ message: 'Produit ajouté avec succès', result });
+
+        conn.end();
+    } catch (err) {
+        console.error('Erreur lors de l\'ajout du produit :', err);
+        res.status(500).json({ error: err });
+    }
+});
+
 module.exports = router;
