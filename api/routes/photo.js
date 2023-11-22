b21c0fd97f742005968cd6f72e19c9f620fbd900
Theo <octheo.quennehen@gmail.com>
2023-11-22 13:34:43 +0100

    route photo

---
+++ api/routes/photo.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const { connectToDatabase } = require('../../conn');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const conn = await connectToDatabase();
+        const groups = await conn.query('SELECT id_photo,ordre,liens,id_produit  FROM Photo');
+
+        res.status(200).json(groups);
+
+        conn.end();
+    } catch (err) {
+        console.error('Erreur lors de la récupération des commande :', err);
+        res.status(500).json({ error: err });
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const {ordre,liens,id_produit} = req.body;
+
+        const conn = await connectToDatabase();
+        const result = await conn.query(
+            'INSERT INTO Photo (ordre,liens,id_produit) VALUES (?, ?, ?)',
+            [ordre,liens,id_produit]
+        );
+
+        res.status(200).json({ message: 'Produit ajouté avec succès', result });
+
+        conn.end();
+    } catch (err) {
+        console.error('Erreur lors de l\'ajout du produit :', err);
+        res.status(500).json({ error: err });
+    }
+});
+
+module.exports = router;
