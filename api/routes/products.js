8586a1d1bc2dc2b71ed3ab6c5d7a6ddc94b68f3e
Theo <octheo.quennehen@gmail.com>
2023-11-28 14:32:37 +0100

    modif

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,21 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const { connectToDatabase } = require('../../conn');
-
-router.get('/categories/:categoryId', async (req, res, next) => {
+router.get('/categories/:identifier', async (req, res, next) => {
     try {
-        const categoryId = req.params.categoryId;
+        const identifier = req.params.identifier;
         const conn = await connectToDatabase();
+
+        let categoryId;
+        if (!isNaN(identifier)) {
+            // Si l'identifiant est numérique, considérons que c'est un ID
+            categoryId = parseInt(identifier);
+        } else {
+            // Si ce n'est pas un nombre, traitons-le comme un nom de catégorie
+            const categoryByName = await conn.query(
+                'SELECT id_categories FROM Categories WHERE nom = ?',
+                [identifier]
+            );
+            if (categoryByName.length === 0) {
+                // Si aucune catégorie n'est trouvée avec ce nom, renvoyer une erreur 404
+                return res.status(404).json({ error: 'Catégorie non trouvée' });
+            }
+            categoryId = categoryByName[0].id_categories;
+        }
+
+        // Fonction récursive pour récupérer toutes les catégories filles
+        async function fetchChildCategories(parentId) {
+            const childCategories = await conn.query(
+                'SELECT id_categories FROM Categories WHERE id_parent = ?',
+                [parentId]
+            );
+            let categories = [];
+            for (const category of childCategories) {
+                categories.push(category.id_categories);
+                const subCategories = await fetchChildCategories(category.id_categories);
+                categories = categories.concat(subCategories);
+            }
+            return categories;
+        }
+
+        // Récupérer toutes les catégories filles de manière récursive
+        const allChildCategories = await fetchChildCategories(categoryId);
+
+        // Ajouter aussi la catégorie parente
+        allChildCategories.push(categoryId);
+
+        // Récupérer les produits associés à toutes les catégories filles et à la catégorie parente
         const products = await conn.query(
             'SELECT id_produit, description, prix, nom, taille, disponibilite, stock, id_categories, id_promotion, id_marque ' +
             'FROM Produit ' +
-            'WHERE id_categories = ?',
-            [categoryId]
-
+            'WHERE id_categories IN (?)',
+            [allChildCategories]
         );
 
         res.status(200).json(products);
-
         conn.end();
     } catch (err) {
         console.error('Erreur lors de la récupération des produits :', err);
