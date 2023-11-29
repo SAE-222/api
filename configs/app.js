149483a7865f7a12ed08d33a6e2b144fbb10ab4d
Theo <octheo.quennehen@gmail.com>
2023-11-29 11:06:48 +0100

    modif générales

--- configs/app.js
+++ configs/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 app.use(express.json());
 
@@ -11,14 +12,22 @@ function errorHandler(err, req, res, next) {
 
 app.use(errorHandler); // Utilisation du gestionnaire d'erreurs
 
-for (const file of fs.readdirSync('./api/routes')) {
+const routesDir = path.join(__dirname, '../routes');
+
+const routeFiles = fs.readdirSync(routesDir);
+
+routeFiles.forEach((file) => {
     try {
         if (file.endsWith('.js')) {
-            app.use('/api/' + file.split('.')[0], require('../routes/' + file));
+            const routeName = file.split('.')[0]; // Nom de la route sans l'extension .js
+            const routePath = `/api/${routeName}`; // Chemin de la route
+
+            const route = require(path.join(routesDir, file));
+            app.use(routePath, route);
         }
     } catch (err) {
         console.error('Une erreur est survenue lors du montage des routes de l\'API :', err);
     }
-}
+});
 
 module.exports = app;
