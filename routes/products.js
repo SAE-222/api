052e2a368c5dedc2acacfc7e2cea525f1aa49b89
Kanta0611 <lucasdumont.60@gmail.com>
2023-11-29 01:43:13 +0100

    Réorganisation de l'arborescence : - `api/routes/` => `routes/` : la racine est déjà le dossier de l'api - `configs/` => fichiers de configurations de connection (conn, app)
    J'ai vérifié :
    - [x] Les include
    - [x] les variables
    - [x] les module exports
    - [ ] le fonctionnement en live

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { connectToDatabase } = require('../../conn');
+const { connectToDatabase } = require('../configs/conn');
 router.get('/categories/:identifier', async (req, res, next) => {
     try {
         const identifier = req.params.identifier;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 
 const http = require('http');
-const app = require('./app');
+const app = require('./configs/app');
 
 const port = process.env.APIPORT || 8393;
 const server = http.createServer(app);
 
 server.on('error', (error) => {
-    console.error('Une erreur s\'est produite lors du démarrage du serveur :', error);
+    console.error('Une erreur s\'est produite lors du démarrage de l\'API :', error);
 });
 
 server.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+    console.log(`API en cours d'exécution sur le port ${port}`);
 });
